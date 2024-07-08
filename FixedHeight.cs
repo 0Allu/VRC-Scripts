1c093313353033b9aa4d477c5f4d3ed9bc02ddc2
Allu <aleksi.v.raisanen@gmail.com>
2024-07-09 02:07:52 +0300

    1.0.0

--- FixedHeight.cs
+++ FixedHeight.cs
@@ -6,7 +6,7 @@ using VRC.Udon;
 public class FixedHeight : UdonSharpBehaviour
 {
     public float SetHeight = 1.8f;
-    void Update() // Loops every frame to keep up with changing avatars.
+    void Update() // Loops every frame to keep up with changing avatars
     {
         Networking.LocalPlayer.SetAvatarEyeHeightByMeters(SetHeight);
         Networking.LocalPlayer.SetAvatarEyeHeightMinimumByMeters(SetHeight);
