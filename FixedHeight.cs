7b1531494ecea9decf8672950a04e50df8af5b23
Allu <aleksi.v.raisanen@gmail.com>
2024-07-09 02:05:44 +0300

    Create FixedHeight.cs

---
+++ FixedHeight.cs
@@ -0,0 +1,15 @@
+using UdonSharp;
+using UnityEngine;
+using VRC.SDKBase;
+using VRC.Udon;
+
+public class FixedHeight : UdonSharpBehaviour
+{
+    public float SetHeight = 1.8f;
+    void Update() // Loops every frame to keep up with changing avatars.
+    {
+        Networking.LocalPlayer.SetAvatarEyeHeightByMeters(SetHeight);
+        Networking.LocalPlayer.SetAvatarEyeHeightMinimumByMeters(SetHeight);
+        Networking.LocalPlayer.SetAvatarEyeHeightMaximumByMeters(SetHeight);
+    }
+}
